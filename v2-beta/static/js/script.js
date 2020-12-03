052af28b84a83d31e547c75fe0b8f7a4caaca045
Wahyu Setianto <wahyusetianto19@gmail.com>
2020-12-04 01:39:53 +0700

    Styling body, setting moving navbar, and footer placement

--- v2-beta/static/js/script.js
+++ v2-beta/static/js/script.js
@@ -1,13 +1,50 @@
-var Person = /** @class */ (function () {
-    function Person(name, job, salary) {
-        this.name = name;
-        this.job = job;
-        this.salary = salary;
+// Main
+// Document Element
+var body = document.getElementById('body');
+var foot = document.getElementById('foot');
+var nav = document.getElementById('nav');
+var nav_move;
+// View 
+var View = /** @class */ (function () {
+    function View() {
     }
-    Person.prototype.get_Info = function () {
-        console.log("Name : " + this.name + " \nJob : " + this.job + " \nSalary : " + this.salary);
+    // Footer Placing : Responsive
+    View.prototype.placeFooter = function () {
+        if (body.offsetTop + body.offsetHeight + foot.offsetHeight > window.innerHeight) {
+            foot.style.bottom = "unset";
+        }
+        else {
+            foot.style.bottom = "0";
+        }
     };
-    return Person;
+    // Moving nav when scroll
+    View.prototype.navMove = function () {
+        if (window.scrollY > nav.offsetTop) {
+            nav_move.style.display = "block";
+        }
+        else {
+            nav_move.style.display = "none";
+        }
+    };
+    // Auto when start page
+    View.prototype.start = function () {
+        this.placeFooter();
+        // Set moving Navbar
+        var clone = nav.cloneNode(true);
+        clone.id = "nav-move";
+        document.getElementById('navigator').appendChild(clone);
+        nav_move = document.getElementById('nav-move');
+        this.navMove();
+    };
+    return View;
 }());
-var wahyu = new Person('Wahyu Setianto', 'Mahasiswa', 0);
-console.log(wahyu.get_Info());
+// Init view and start
+var view = new View();
+view.start();
+// Listener
+window.addEventListener('resize', function () {
+    view.placeFooter();
+});
+window.addEventListener('scroll', function () {
+    view.navMove();
+});
--- v2-beta/static/ts/script.ts
+++ v2-beta/static/ts/script.ts
@@ -1,18 +1,54 @@
-class Person{
-    name:string;
-    job:string;
-    salary:number;
+// Main
+// Document Element
+const body:any = document.getElementById('body');
+const foot:any = document.getElementById('foot');
+const nav:any = document.getElementById('nav');
+let nav_move:any;
 
-    constructor(name:string, job:string, salary:number){
-        this.name = name;
-        this.job = job;
-        this.salary = salary;
+// View 
+class View{
+
+    // Footer Placing : Responsive
+    placeFooter() : void{
+        if(body.offsetTop + body.offsetHeight + foot.offsetHeight > window.innerHeight){
+            foot.style.bottom = "unset";
+        }else{
+            foot.style.bottom = "0";
+        }
     }
 
-    get_Info(){
-        console.log(`Name : ${this.name} \nJob : ${this.job} \nSalary : ${this.salary}`);
+    // Moving nav when scroll
+    navMove() : void{
+        if(window.scrollY > nav.offsetTop){
+            nav_move.style.display = "block";
+        }else{
+            nav_move.style.display = "none";
+        }
+    }
+
+    // Auto when start page
+    start() : void{
+        this.placeFooter();
+        
+        // Set moving Navbar
+        const clone = nav.cloneNode(true);
+        clone.id = "nav-move";
+        document.getElementById('navigator').appendChild(clone);
+        nav_move = document.getElementById('nav-move');
+
+        this.navMove();
     }
 }
 
-const wahyu = new Person('Wahyu Setianto', 'Mahasiswa', 0);
-console.log(wahyu.get_Info());
+// Init view and start
+const view : View = new View();
+view.start();
+
+// Listener
+window.addEventListener('resize', () : void => {
+    view.placeFooter();
+})
+
+window.addEventListener('scroll', () : void => {
+    view.navMove();
+});
