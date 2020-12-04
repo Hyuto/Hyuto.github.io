9e27237f80d4804730ceff2f929771778189fd04
Wahyu Setianto <wahyusetianto19@gmail.com>
2020-12-05 01:29:08 +0700

    Set index.html to automatically getting notebook API

--- v2-beta/static/js/script.js
+++ v2-beta/static/js/script.js
@@ -1,3 +1,39 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 // Main
 // Document Element
 var body = document.getElementById('body');
@@ -6,8 +42,36 @@ var nav = document.getElementById('nav');
 var nav_move;
 // View 
 var View = /** @class */ (function () {
-    function View() {
+    function View(url) {
+        var _this = this;
+        this.GetData = function (url) { return __awaiter(_this, void 0, void 0, function () {
+            var data;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, fetch(url).then(function (res) {
+                            return res.json();
+                        })];
+                    case 1:
+                        data = _a.sent();
+                        return [2 /*return*/, data];
+                }
+            });
+        }); };
+        this.url = url;
     }
+    View.fillContent = function (content, url) {
+        var container = document.createElement('div');
+        var head = document.createElement('h1');
+        var text = document.createElement('p');
+        //Set content
+        head.innerText = content;
+        text.innerHTML = "link : <a href=\"" + url + "\">" + content + "</a>";
+        container.appendChild(head);
+        container.appendChild(text);
+        container.className = 'box box-1';
+        //Inject to body
+        body.appendChild(container);
+    };
     // Footer Placing : Responsive
     View.prototype.placeFooter = function () {
         if (body.offsetTop + body.offsetHeight + foot.offsetHeight > window.innerHeight) {
@@ -28,18 +92,30 @@ var View = /** @class */ (function () {
     };
     // Auto when start page
     View.prototype.start = function () {
-        this.placeFooter();
-        // Set moving Navbar
-        var clone = nav.cloneNode(true);
-        clone.id = "nav-move";
-        document.getElementById('navigator').appendChild(clone);
-        nav_move = document.getElementById('nav-move');
-        this.navMove();
+        var _this = this;
+        // Set Content
+        this.data = this.GetData(this.url);
+        this.data.then(function (e) {
+            e.forEach(function (element) {
+                // Inject to Body
+                View.fillContent(element.name, element.url);
+            });
+        }).then(function () {
+            // Place footer
+            _this.placeFooter();
+            // Set moving Navbar
+            var clone = nav.cloneNode(true);
+            clone.id = "nav-move";
+            document.getElementById('navigator').appendChild(clone);
+            nav_move = document.getElementById('nav-move');
+            // Placement
+            _this.navMove();
+        });
     };
     return View;
 }());
 // Init view and start
-var view = new View();
+var view = new View("https://hyuto.github.io/notebooks/API/API.json");
 view.start();
 // Listener
 window.addEventListener('resize', function () {
--- v2-beta/static/ts/script.ts
+++ v2-beta/static/ts/script.ts
@@ -7,6 +7,33 @@ let nav_move:any;
 
 // View 
 class View{
+    url:string;
+    data:any;
+
+    constructor(url:string){
+        this.url = url;
+    }
+
+    GetData = async (url:string) => {
+        const data = await fetch(url).then((res) => {
+            return res.json();
+        });
+        return data;
+    }
+
+    public static fillContent(content:string, url:string) : void{
+        const container:any = document.createElement('div');
+        const head:any = document.createElement('h1');
+        const text:any = document.createElement('p');
+        //Set content
+        head.innerText = content;
+        text.innerHTML = `link : <a href="${url}">${content}</a>`
+        container.appendChild(head);
+        container.appendChild(text);
+        container.className = 'box box-1';
+        //Inject to body
+        body.appendChild(container);
+    }
 
     // Footer Placing : Responsive
     placeFooter() : void{
@@ -28,20 +55,30 @@ class View{
 
     // Auto when start page
     start() : void{
-        this.placeFooter();
+        // Set Content
+        this.data = this.GetData(this.url);
+        this.data.then((e) => {
+            e.forEach(element => {
+                // Inject to Body
+                View.fillContent(element.name, element.url);
+            });
+        }).then(() => {
+            // Place footer
+            this.placeFooter();
         
-        // Set moving Navbar
-        const clone = nav.cloneNode(true);
-        clone.id = "nav-move";
-        document.getElementById('navigator').appendChild(clone);
-        nav_move = document.getElementById('nav-move');
-
-        this.navMove();
+            // Set moving Navbar
+            const clone = nav.cloneNode(true);
+            clone.id = "nav-move";
+            document.getElementById('navigator').appendChild(clone);
+            nav_move = document.getElementById('nav-move');
+            // Placement
+            this.navMove();
+        })
     }
 }
 
 // Init view and start
-const view : View = new View();
+const view : View = new View("https://hyuto.github.io/notebooks/API/API.json");
 view.start();
 
 // Listener
