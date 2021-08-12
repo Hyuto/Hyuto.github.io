1e98a0a7a8a599a557351f464e27fa210f5c41c2
Hyuto <wahyusetianto19@gmail.com>
2021-08-13 01:06:22 +0700

    Updates

---
+++ component---src-pages-404-js-7d7e4be9ae31018f7e40.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkHyuto_github_io=self.webpackChunkHyuto_github_io||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),u=a(7198),o=a(3751);e.default=function(t){var e=t.data,a=t.location,i=e.site.siteMetadata.title;return n.createElement(u.Z,{location:a,title:i},n.createElement(o.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7d7e4be9ae31018f7e40.js.map
