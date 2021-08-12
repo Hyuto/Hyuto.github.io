3c280e63fd971faf5b3274f3eacd9cd362acb13a
Hyuto <wahyusetianto19@gmail.com>
2021-08-13 01:03:15 +0700

    Archiving v1 and v2 & Move to v3

---
+++ v3-beta/src/components/bio.js
@@ -0,0 +1,55 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
+
+const Bio = () => {
+  const data = useStaticQuery(graphql`
+    query BioQuery {
+      site {
+        siteMetadata {
+          author {
+            name
+            summary
+          }
+        }
+      }
+    }
+  `)
+
+  // Set these values by editing "siteMetadata" in gatsby-config.js
+  const author = data.site.siteMetadata?.author
+
+  return (
+    <div className="bio">
+      <Link to="/about">
+        <StaticImage
+          className="bio-avatar"
+          layout="fixed"
+          formats={["AUTO", "WEBP", "AVIF"]}
+          src="../images/profile-pic.png"
+          width={50}
+          height={50}
+          quality={95}
+          alt="Profile picture"
+        />
+      </Link>
+      {author?.name && (
+        <p>
+          Written by <strong>{author.name}</strong> {author?.summary || null}
+          {` `}
+          <Link to="/about">About Me</Link>
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default Bio
