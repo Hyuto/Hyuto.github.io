a6d76a561981ec0b8823777a92da54f4b86a8fc8
Hyuto <wahyusetianto19@gmail.com>
2021-08-23 13:35:26 +0700

    Add firebase connection

--- v3-beta/gatsby-browser.js
+++ v3-beta/gatsby-browser.js
@@ -1,10 +1,17 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+
 // normalize CSS across browsers
 import "./src/normalize.css"
+
 // custom CSS styles
 import "./src/style.css"
+
+// firebase
+import "firebase/auth"
+import "firebase/firestore"
+import "firebase/functions"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
--- v3-beta/gatsby-config.js
+++ v3-beta/gatsby-config.js
@@ -64,12 +64,20 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    //{
+    //  resolve: "gatsby-plugin-firebase",
+    //  options: {
+    //    credentials: {
+    //      apiKey: "<YOUR_FIREBASE_API_KEY>",
+    //      authDomain: "<YOUR_FIREBASE_AUTH_DOMAIN>",
+    //      databaseURL: "<YOUR_FIREBASE_DATABASE_URL>",
+    //      projectId: "<YOUR_FIREBASE_PROJECT_ID>",
+    //      storageBucket: "<YOUR_FIREBASE_STORAGE_BUCKET>",
+    //      messagingSenderId: "<YOUR_FIREBASE_MESSAGING_SENDER_ID>",
+    //      appId: "<YOUR_FIREBASE_APP_ID>",
+    //    },
+    //  },
+    //},
     {
       resolve: `gatsby-plugin-feed`,
       options: {
