eed3faf2dabed0e0360f3416d324146d50b94155
Hyuto <wahyusetianto19@gmail.com>
2021-09-10 23:54:56 +0700

    Using gatsby image

--- v3-beta/src/components/tabbed-images/tabbed-images.js
+++ v3-beta/src/components/tabbed-images/tabbed-images.js
@@ -1,9 +1,45 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { useLocation } from "@reach/router";
+import Img from "gatsby-image";
 
-const TabbedImages = ({ children }) => {
-  console.log(children);
+const TabbedImages = ({ names }) => {
+  const location = useLocation();
+  const data = useStaticQuery(graphql`
+    query {
+      allFile(filter: { extension: { eq: "png" } }) {
+        edges {
+          node {
+            relativeDirectory
+            childrenImageSharp {
+              fluid(maxWidth: 630) {
+                ...GatsbyImageSharpFluid
+                originalName
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+  const dir = location.pathname.split("/").join("");
+  const images = data.allFile.edges.filter(e => {
+    if (
+      e.node.relativeDirectory === dir &&
+      names.includes(e.node.childrenImageSharp[0].fluid.originalName)
+    ) {
+      return true;
+    }
+    return false;
+  });
 
-  return <div>{children}</div>;
+  return (
+    <div>
+      {images.map(e => {
+        return <Img fluid={e.node.childrenImageSharp[0].fluid} />;
+      })}
+    </div>
+  );
 };
 
 export default TabbedImages;
