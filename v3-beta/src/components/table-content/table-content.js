b6312d439cbacf2f18df93683cbe6673c8cf6049
Hyuto <wahyusetianto19@gmail.com>
2021-09-25 23:25:53 +0700

    Trying to make TOC clickable in mobile view

--- v3-beta/src/components/table-content/table-content.js
+++ v3-beta/src/components/table-content/table-content.js
@@ -1,10 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Hamburger from "hamburger-react";
 import * as style from "./table-content.module.scss";
 
 const TableContent = ({ headings }) => {
-  if (headings === undefined) return null;
-  if (headings.length === 0) return null;
-
   const data = headings.map(e => {
     const lower = e.value.toLowerCase();
     const punctless = lower.replace(/[.,'/#!$%^&*;:{}=_`~()]/g, "");
@@ -14,19 +12,43 @@ const TableContent = ({ headings }) => {
     };
   });
 
+  const [visible, setVisibility] = useState(null);
+
+  useEffect(() => {
+    if (visible !== null) {
+      const toggleVisibility = () => {
+        if (window.screen.width >= 1350) setVisibility(true);
+        else setVisibility(false);
+      };
+      window.addEventListener("resize", toggleVisibility);
+      return () => window.removeEventListener("resize", toggleVisibility);
+    } else setVisibility(window.screen.width >= 1300);
+  }, [visible]);
+
   return (
-    <div className={style.wrapper}>
-      <div className={style.title}>Table of Contents</div>
-      <div className={style.list}>
-        {data.map((e, index) => {
-          return (
-            <li key={`table-contents-${index}`} style={{ margin: "2px 0" }}>
-              {index + 1}. <a href={`#${e.className}`}>{e.title}</a>
-            </li>
-          );
-        })}
-      </div>
-    </div>
+    <>
+      {headings && headings.length > 0 && (
+        <div className={style.wrapper}>
+          <div className={style.hamburger}>
+            <Hamburger size={20} />
+          </div>
+          {visible && (
+            <div className={style.main}>
+              <div className={style.title}>Table of Contents</div>
+              <div className={style.list}>
+                {data.map((e, index) => {
+                  return (
+                    <li key={`table-contents-${index}`} style={{ margin: "2px 0" }}>
+                      {index + 1}. <a href={`#${e.className}`}>{e.title}</a>
+                    </li>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </>
   );
 };
 
