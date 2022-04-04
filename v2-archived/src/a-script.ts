76ab1f326767e3efd2dc440b8b0f03f4e01886dc
Hyuto <wahyusetianto19@gmail.com>
2022-04-04 23:50:58 +0700

    Use parcel bundler for v2

---
+++ v2-archived/src/a-script.ts
@@ -0,0 +1,15 @@
+import { View, CloneElement } from "./view";
+
+class HomeView extends View {
+  start() {
+    this._ContentPlacement();
+  }
+}
+
+const body: HTMLElement = document.getElementById("body");
+const foot: HTMLElement = document.getElementById("foot");
+const nav: HTMLElement = document.getElementById("nav");
+const nav_move: HTMLElement = CloneElement(nav, "nav-move", "navigator");
+
+const Home: HomeView = new HomeView(body, foot, nav, nav_move);
+Home.start();
