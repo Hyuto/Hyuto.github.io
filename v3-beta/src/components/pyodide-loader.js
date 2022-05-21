4806c5df98714a894577e2cb5cf4ddb186399ad1
Hyuto <wahyusetianto19@gmail.com>
2022-05-22 00:15:23 +0700

    Add pyodide support to run python code in browser

---
+++ v3-beta/src/components/pyodide-loader.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Helmet from "react-helmet";
+
+/**
+ * Load pyodide v0.20.0 via CDN
+ */
+const PyodideLoader = ({ setLoad }) => {
+  return (
+    <>
+      <Helmet
+        script={[{ src: "https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js" }]}
+        onChangeClientState={(newState, addedTags) => {
+          if (addedTags.scriptTags) {
+            addedTags.scriptTags[0].onload = () => {
+              console.log("pyodide loaded!");
+              setLoad("loaded");
+            };
+          }
+        }}
+      />
+    </>
+  );
+};
+
+export default PyodideLoader;
